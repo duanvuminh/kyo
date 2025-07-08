0ad870aebfb969a51e903a8c618fab3ab51627bc
duan-guide <duan@guide.inc>
2025-07-08 10:52:10 +0900

    update schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { ChatWidget } from "@/components/chat-widget";
 import { MdxWrapperStyle } from "@/components/mdx-wrapper-style";
+import SchemeLinkGenerator from "@/components/scheme_link_generator";
 
 export default function Page() {
   return (
     <MdxWrapperStyle>
       <ChatWidget />
+      <SchemeLinkGenerator />
     </MdxWrapperStyle>
   );
 }
