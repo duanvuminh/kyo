0929199f1981efc1dc21b133689233b675bf9a92
duan vu minh <duan@guide.inc>
2025-11-23 11:43:27 +0900

    Add Flash Card page and update grammar structure in N1 Page 2
    - Created a new Flash Card page component to display grammar cards.
    - Updated the N1 Page 2 MDX file to include a table of grammar structures with their meanings in Vietnamese.
    - Added a link to the Flash Card page for easy navigation.

---
+++ app/grammar/n1/page2/flash-card/page.tsx
@@ -0,0 +1,13 @@
+import { cards } from "@/app/grammar/n1/page2/flash-card/cards";
+import { FlashCard } from "@/shared/component/flash-card/flash-card";
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold text-center mt-8">
+        Flash Card Grammar N1 - Page 2
+      </h1>
+      <FlashCard cards={cards} />
+    </main>
+  );
+}
